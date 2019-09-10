9fafd8290b35a2d42a27f3ce429247fe93f2265e
Teo Asinari <teoasinari@Teos-MacBook-Pro.local>
2019-09-09 18:13:45 -0700

    I have rudimentary webpack running

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Teo's First Visualization in ThreeJS
-Not much to say here, yet.
+
+##To run:
+1. clone this repo
+2. `npm install`
+`npx webpack`
+3. view dist/index.html in your browser.
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
+
+let scene = new Scene();
+
+let fov = 75;
+let aspect = window.innerWidth / window.innerHeight;
+let near = 0.1;
+let far = 1000;
+
+let camera = new PerspectiveCamera(fov, aspect, near, far);
+let renderer = new WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let geometry = new BoxGeometry(1, 1, 1);
+let material = new MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new Mesh( geometry, material );
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
