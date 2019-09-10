aeb04040960bc670f349c0791a516ed396ef4134
Teo Asinari <teoasinari@Teos-MacBook-Pro.local>
2019-09-09 20:17:05 -0700

    removing unnecessary files and adding .gitignore file

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 ## To run:
 1. Clone this repo
 2. At the project root: `npm install`
-`npm run build`
+`npm run start`
 3. View dist/index.html in your browser.
+
+## For development
+1. `npm install -g nodemon` 
+`nodemon --ignore ./dist --exec npm run start`
--- src/index.js
+++ src/index.js
@@ -14,16 +14,28 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let geometry = new BoxGeometry(1, 1, 1);
+geometry.translate(-1, 1,1);
 let material = new MeshBasicMaterial({ color: 0x00ff00 });
 let cube = new Mesh( geometry, material );
+
+
+
+// for (let horiz_index = 0; horiz_index <= 5; horiz_index++) {
+//     let geometry = new BoxGeometry(1, 1, 1);
+//     geometry.translate(horiz_index, 1, 1);
+//     let material = new MeshBasicMaterial({ color: 0x00ff00 });
+//     let cube = new Mesh( geometry, material );
+//     scene.add(cube);
+// }
+
 scene.add(cube);
 
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
